d7d15054e573afaa6e754582d195c974e1f7c78a
gauravborse1210 <gauravborse1210@gmail.com>
2025-11-14 16:53:13 +0530

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import customerReducer from "./features/customers/customerSlice";
+import accountReducer from "./features/accounts/accountSlice";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
+
+export default store;
